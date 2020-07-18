6bd2227b0aad5ecc51d55f5ca6cdca5992bf6c8b
wantanmee123 <mengkeonglee55@gmail.com>
2020-07-18 13:41:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practical_4_aboutme_linearlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.practical_4_aboutme_linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
